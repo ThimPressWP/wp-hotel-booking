2bcc080a183cb65cbafac464f1b2409c5bcdcfd7
Tu <tunn@foobla.com>
2015-07-28 11:44:00 +0700

    TP Hotel Booking

---
+++ tp-hotel-booking.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: TP Hotel Booking
+Plugin URI: http://thimpress.com/learnpress
+Description: [Description here]
+Author: ThimPress
+Version: 0.9
+Author URI: http://thimpress.com
+*/
