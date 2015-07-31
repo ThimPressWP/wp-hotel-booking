60d48c9759d2c374e99371ccf059191c48a678f3
halink0803 <halink0803@gmail.com>
2015-07-31 12:45:29 +0700

    edit label

--- includes/admin/views/pricing-table.php
+++ includes/admin/views/pricing-table.php
@@ -1,7 +1,7 @@
 <div class="wrap">
     <h2><?php _e( 'Pricing Plan', 'tp-hotel-booking' );?></h2>
     <form method="post">
-        <p><?php _e( 'Select room', 'tp-hotel-booking' );?><?php hb_dropdown_room_types();?></p>
+        <p><?php _e( 'Select room type', 'tp-hotel-booking' );?><?php hb_dropdown_room_types();?></p>
 
         <p>
             <button class="button button-primary"><?php _e( 'Update', 'tp-hotel-booking' );?></button>
