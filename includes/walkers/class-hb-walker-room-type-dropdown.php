9d1af5a2be4a0565fb26a20f01fe5bc1d246fb83
Tu <tunn@foobla.com>
2015-08-11 09:34:59 +0700

    TP Hotel Booking

--- includes/walkers/class-hb-walker-room-type-dropdown.php
+++ includes/walkers/class-hb-walker-room-type-dropdown.php
@@ -1,8 +1,6 @@
 <?php
 class HB_Walker_Room_Type_Dropdown extends Walker_CategoryDropdown {
-    function start_el(&$output, $category, $depth, $args) {
-
-        echo 'YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY';
+    function start_el(&$output, $category, $depth = 0, $args = array(), $id = 0) {
         $pad = str_repeat('&nbsp;', $depth * 3);
 
         $cat_name = apply_filters('list_cats', $category->name, $category);
