554241778782fd70da5c5c923482455d7d172cbb
ducnvtt92 <duc@foobla.com>
2015-10-09 17:47:14 +0700

    quen ko add file =))

---
+++ templates/loop/mini-cart-loop.php
@@ -0,0 +1,22 @@
+<div class="hb_mini_cart_item" data-search-key="<?php echo $room->in_to_out; ?>" data-id="<?php echo $room->ID ?>">
+
+	<div class="hb_mini_cart_top">
+
+		<h4><?php printf( '%s (%s)', $room->name, $room->capacity_title ) ?></h4>
+
+	</div>
+
+	<div class="hb_mini_cart_number">
+
+		<label><?php _e( 'Number of room: ', 'tp-hotel-booking' ); ?></label>
+		<span><?php printf( '%s', $room->quantity );  ?></span>
+
+	</div>
+
+	<div class="hb_mini_cart_number">
+
+		<label><?php _e( 'Price: ', 'tp-hotel-booking' ); ?></label>
+		<span><?php printf( '%s', hb_format_price( $room->total ) ) ?></span>
+
+	</div>
+</div>
---
+++ templates/mini_cart.php
@@ -0,0 +1,17 @@
+<?php
+	global $hb_cart;
+
+	$rooms = $hb_cart->get_rooms();
+?>
+
+<?php foreach ($rooms as $key => $room): ?>
+
+	<?php hb_get_template( 'loop/mini-cart-loop.php', array( 'room'	=> $room ) ) ?>
+
+<?php endforeach; ?>
+<div class="hb_mini_cart_footer">
+
+	<a href="<?php echo hb_get_url(array( 'hotel-booking' => 'checkout')) ?>" class="hb_button hb_checkout"><?php _e( 'Check Out', 'tp-hotel-booking' );?></a>
+	<a href="<?php echo hb_get_url( array('hotel-booking' => 'cart') ); ?>" class="hb_button hb_view_cart"><?php _e( 'View Cart', 'tp-hotel-booking' );?></a>
+
+</div>
