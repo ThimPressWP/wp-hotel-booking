09dc7565b2ddafa57dfde7fb8a029cbdee9732cf
ducnvtt92 <duc@foobla.com>
2015-11-17 14:45:07 +0700

    update shortcode search

--- tp-hotel-booking.php
+++ tp-hotel-booking.php
@@ -4,14 +4,14 @@
     Plugin URI: http://thimpress.com/learnpress
     Description: [Description here]
     Author: ThimPress
-    Version: 0.9
+    Version: 1.0
     Author URI: http://thimpress.com
 */
 
 define( 'HB_FILE', __FILE__ );
 define( 'HB_PLUGIN_PATH', dirname( __FILE__ ) );
 define( 'HB_PLUGIN_URL', plugins_url( '', __FILE__ ) );
-define( 'HB_VERSION', '0.9' );
+define( 'HB_VERSION', '1.0' );
 define( 'HB_BLOG_ID', get_current_blog_id() );
 /**
  * Class TP_Hotel_Booking
