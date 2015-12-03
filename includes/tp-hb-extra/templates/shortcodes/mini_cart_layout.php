3439e22653bfe7cdcdf6df2c27469f1cbfcf857f
ducnvtt92 <duc@foobla.com>
2015-12-03 14:16:55 +0700

    update url cart, checkout

--- includes/tp-hb-extra/templates/shortcodes/mini_cart_layout.php
+++ includes/tp-hb-extra/templates/shortcodes/mini_cart_layout.php
@@ -47,8 +47,8 @@
 <script type="text/html" id="tmpl-hb-minicart-footer">
     <div class="hb_mini_cart_footer">
 
-        <a href="<?php echo hb_get_url(array( 'hotel-booking' => 'checkout')) ?>" class="hb_button hb_checkout"><?php _e( 'Check Out', 'tp-hotel-booking' );?></a>
-        <a href="<?php echo hb_get_url( array('hotel-booking' => 'cart') ); ?>" class="hb_button hb_view_cart"><?php _e( 'View Cart', 'tp-hotel-booking' );?></a>
+        <a href="<?php echo hb_get_checkout_url() ?>" class="hb_button hb_checkout"><?php _e( 'Check Out', 'tp-hotel-booking' );?></a>
+        <a href="<?php echo hb_get_cart_url() ?>" class="hb_button hb_view_cart"><?php _e( 'View Cart', 'tp-hotel-booking' );?></a>
 
     </div>
 </script>
