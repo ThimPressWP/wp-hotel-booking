ef596776f4c360883f6c99ef36303b837990bdb7
ducnvtt92 <duc@foobla.com>
2016-07-31 22:45:57 +0700

    fix bug. conflict booking in single room vs block special date

--- includes/hb-core-functions.php
+++ includes/hb-core-functions.php
@@ -1,76 +1,78 @@
 <?php
+
 /**
  * @Author: ducnvtt
  * @Date:   2016-04-01 09:45:55
  * @Last Modified by:   someone
  * @Last Modified time: 2016-05-13 15:23:06
  */
-
 /**
  * set table name.
  */
+if ( !function_exists( 'hotel_booking_set_table_name' ) ) {
 
-if ( ! function_exists( 'hotel_booking_set_table_name' ) ) {
-	function hotel_booking_set_table_name() {
-		global $wpdb;
-		$order_item = 'hotel_booking_order_items';
-		$order_itemmeta = 'hotel_booking_order_itemmeta';
-		$hotel_booking_plans = 'hotel_booking_plans';
+    function hotel_booking_set_table_name() {
+        global $wpdb;
+        $order_item = 'hotel_booking_order_items';
+        $order_itemmeta = 'hotel_booking_order_itemmeta';
+        $hotel_booking_plans = 'hotel_booking_plans';
 
-		$wpdb->hotel_booking_order_items = $wpdb->prefix . $order_item;
-		$wpdb->hotel_booking_order_itemmeta = $wpdb->prefix . $order_itemmeta;
-		$wpdb->hotel_booking_plans = $wpdb->prefix . $hotel_booking_plans;
+        $wpdb->hotel_booking_order_items = $wpdb->prefix . $order_item;
+        $wpdb->hotel_booking_order_itemmeta = $wpdb->prefix . $order_itemmeta;
+        $wpdb->hotel_booking_plans = $wpdb->prefix . $hotel_booking_plans;
 
-		$wpdb->tables[] = 'hotel_booking_order_items';
-		$wpdb->tables[] = 'hotel_booking_order_itemmeta';
-		$wpdb->tables[] = 'hotel_booking_plans';
-	}
-	add_action( 'init', 'hotel_booking_set_table_name', 0 );
-	add_action( 'switch_blog', 'hotel_booking_set_table_name', 0 );
+        $wpdb->tables[] = 'hotel_booking_order_items';
+        $wpdb->tables[] = 'hotel_booking_order_itemmeta';
+        $wpdb->tables[] = 'hotel_booking_plans';
+    }
+
+    add_action( 'init', 'hotel_booking_set_table_name', 0 );
+    add_action( 'switch_blog', 'hotel_booking_set_table_name', 0 );
 }
 
-if ( ! function_exists( 'hotel_booking_get_room_available' ) ) {
-	function hotel_booking_get_room_available( $room_id = null, $args = array() ) {
-		$valid = true;
-		$errors = new WP_Error;
-		if ( ! $room_id ) {
-			$valid = false;
-			$errors->add( 'room_id_invalid', __( 'Room not found.', 'tp-hotel-booking' ) );
-		}
+if ( !function_exists( 'hotel_booking_get_room_available' ) ) {
 
-		$args = wp_parse_args( $args, array(
-				'check_in_date'		=> '',
-				'check_out_date'	=> '',
-				'excerpt'			=> array(
-						0
-					)
-			));
+    function hotel_booking_get_room_available( $room_id = null, $args = array() ) {
+        $valid = true;
+        $errors = new WP_Error;
+        if ( !$room_id ) {
+            $valid = false;
+            $errors->add( 'room_id_invalid', __( 'Room not found.', 'tp-hotel-booking' ) );
+        }
 
-		if ( ! $args[ 'check_in_date' ] ) {
-			$valid = false;
-			$errors->add( 'check_in_date_not_available', __( 'Check in date is not valid.', 'tp-hotel-booking' ) );
-		} else {
-			if ( ! is_numeric( $args[ 'check_in_date' ] ) ) {
-				$args[ 'check_in_date' ] = strtotime( $args[ 'check_in_date' ] );
-			}
-		}
+        $args = wp_parse_args( $args, array(
+            'check_in_date' => '',
+            'check_out_date' => '',
+            'excerpt' => array(
+                0
+            )
+        ) );
 
-		if ( ! $args[ 'check_out_date' ] ) {
-			$valid = false;
-			$errors->add( 'check_out_date_not_available', __( 'Check out date is not valid.', 'tp-hotel-booking' ) );
-		} else {
-			if ( ! is_numeric( $args[ 'check_out_date' ] ) ) {
-				$args[ 'check_out_date' ] = strtotime( $args[ 'check_out_date' ] );
-			}
-		}
+        if ( !$args['check_in_date'] ) {
+            $valid = false;
+            $errors->add( 'check_in_date_not_available', __( 'Check in date is not valid.', 'tp-hotel-booking' ) );
+        } else {
+            if ( !is_numeric( $args['check_in_date'] ) ) {
+                $args['check_in_date'] = strtotime( $args['check_in_date'] );
+            }
+        }
 
-		// $valid is false
-		if ( $valid === false ) {
-			return $errors;
-		} else {
-			global $wpdb;
+        if ( !$args['check_out_date'] ) {
+            $valid = false;
+            $errors->add( 'check_out_date_not_available', __( 'Check out date is not valid.', 'tp-hotel-booking' ) );
+        } else {
+            if ( !is_numeric( $args['check_out_date'] ) ) {
+                $args['check_out_date'] = strtotime( $args['check_out_date'] );
+            }
+        }
 
-			$not = $wpdb->prepare("
+        // $valid is false
+        if ( $valid === false ) {
+            return $errors;
+        } else {
+            global $wpdb;
+
+            $not = $wpdb->prepare( "
 					SELECT SUM( meta.meta_value ) FROM {$wpdb->hotel_booking_order_itemmeta} AS meta
 						LEFT JOIN {$wpdb->hotel_booking_order_items} AS order_item ON order_item.order_item_id = meta.hotel_booking_order_item_id
 						LEFT JOIN {$wpdb->hotel_booking_order_itemmeta} AS room ON order_item.order_item_id = room.hotel_booking_order_item_id
@@ -91,15 +93,10 @@ if ( ! function_exists( 'hotel_booking_get_room_available' ) ) {
 						AND booking.post_type = %s
 						AND booking.post_status IN ( %s, %s, %s )
 						AND order_item.order_id NOT IN( %s )
-				", 'qty', $room_id, 'product_id', 'check_in_date', 'check_out_date',
-					$args['check_in_date'], $args['check_out_date'],
-					$args['check_in_date'], $args['check_out_date'],
-					$args['check_in_date'], $args['check_out_date'],
-					'hb_booking', 'hb-completed', 'hb-processing', 'hb-pending',
-					implode( ',' , $args['excerpt'] )
-				);
+				", 'qty', $room_id, 'product_id', 'check_in_date', 'check_out_date', $args['check_in_date'], $args['check_out_date'], $args['check_in_date'], $args['check_out_date'], $args['check_in_date'], $args['check_out_date'], 'hb_booking', 'hb-completed', 'hb-processing', 'hb-pending', implode( ',', $args['excerpt'] )
+            );
 
-			$sql = $wpdb->prepare("
+            $sql = $wpdb->prepare( "
 					SELECT number.meta_value AS qty FROM $wpdb->postmeta AS number
 						INNER JOIN $wpdb->posts AS hb_room ON hb_room.ID = number.post_id
 					WHERE
@@ -108,25 +105,26 @@ if ( ! function_exists( 'hotel_booking_get_room_available' ) ) {
 						AND hb_room.post_type = %s
 				", '_hb_num_of_rooms', $room_id, 'hb_room' );
 
-			$qty = absint( $wpdb->get_var( $sql ) ) - absint( $wpdb->get_var( $not ) );
-			if ( $qty === 0 ) {
-				$errors->add( 'zero', __( 'This room is not available.', 'tp-hotel-booking' ) );
-				return $errors;
-			}
-			return $qty;
-		}
-	}
+            $qty = absint( $wpdb->get_var( $sql ) ) - absint( $wpdb->get_var( $not ) );
+            if ( $qty === 0 ) {
+                $errors->add( 'zero', __( 'This room is not available.', 'tp-hotel-booking' ) );
+                return $errors;
+            }
+            return apply_filters( 'hotel_booking_get_room_available', $qty, $room_id, $args );
+        }
+    }
+
 }
 
 // product class process
-if ( ! function_exists( 'hotel_booking_get_product_class' ) ) {
+if ( !function_exists( 'hotel_booking_get_product_class' ) ) {
 
-	function hotel_booking_get_product_class( $product_id = null, $params = array() ) {
+    function hotel_booking_get_product_class( $product_id = null, $params = array() ) {
 
         $post_type = get_post_type( $product_id );
 
         $product = 'HB_Product_' . implode( '_', array_map( 'ucfirst', explode( '_', $post_type ) ) );
-        if( ! class_exists( $product ) ) {
+        if ( !class_exists( $product ) ) {
             $product = 'HB_Room';
         }
 
@@ -134,20 +132,19 @@ if ( ! function_exists( 'hotel_booking_get_product_class' ) ) {
         $product = new $product( $product_id, $params );
 
         return apply_filters( 'hotel_booking_get_product_class', $product, $product_id, $params );
-
-	}
+    }
 
 }
 
 function hb_create_page( $slug, $option = '', $page_title = '', $page_content = '', $post_parent = 0 ) {
     global $wpdb;
 
-    $option_value     = get_option( $option );
+    $option_value = get_option( $option );
 
     if ( $option_value > 0 ) {
         $page_object = get_post( $option_value );
 
-        if ( $page_object && 'page' === $page_object->post_type && ! in_array( $page_object->post_status, array( 'pending', 'trash', 'future', 'auto-draft' ) ) ) {
+        if ( $page_object && 'page' === $page_object->post_type && !in_array( $page_object->post_status, array( 'pending', 'trash', 'future', 'auto-draft' ) ) ) {
             // Valid page is already in place
             return $page_object->ID;
         }
@@ -180,21 +177,21 @@ function hb_create_page( $slug, $option = '', $page_title = '', $page_content = 
     }
 
     if ( $trashed_page_found ) {
-        $page_id   = $trashed_page_found;
+        $page_id = $trashed_page_found;
         $page_data = array(
-            'ID'             => $page_id,
-            'post_status'    => 'publish',
+            'ID' => $page_id,
+            'post_status' => 'publish',
         );
         wp_update_post( $page_data );
     } else {
         $page_data = array(
-            'post_status'    => 'publish',
-            'post_type'      => 'page',
-            'post_author'    => 1,
-            'post_name'      => $slug,
-            'post_title'     => $page_title,
-            'post_content'   => $page_content,
-            'post_parent'    => $post_parent,
+            'post_status' => 'publish',
+            'post_type' => 'page',
+            'post_author' => 1,
+            'post_name' => $slug,
+            'post_title' => $page_title,
+            'post_content' => $page_content,
+            'post_parent' => $post_parent,
             'comment_status' => 'closed'
         );
         $page_id = wp_insert_post( $page_data );
