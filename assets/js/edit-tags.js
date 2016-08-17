2936550536b885f6242b15ac5295404a4ac7268a
ducnvtt92 <duc@foobla.com>
2016-08-17 16:50:13 +0700

    Use Gulp

--- assets/js/edit-tags.js
+++ assets/js/edit-tags.js
@@ -1,24 +1,25 @@
-;(function($){
-    function parseJSON(data){
-        if( ! $.isPlainObject(data) ){
-            var m = data.match(/<!-- HB_AJAX_START -->(.*)<!-- HB_AJAX_END -->/);
+;
+( function ( $ ) {
+    function parseJSON( data ) {
+        if ( !$.isPlainObject( data ) ) {
+            var m = data.match( /<!-- HB_AJAX_START -->(.*)<!-- HB_AJAX_END -->/ );
             try {
-                if (m) {
-                    data = $.parseJSON(m[1]);
+                if ( m ) {
+                    data = $.parseJSON( m[1] );
                 } else {
-                    data = $.parseJSON(data);
+                    data = $.parseJSON( data );
                 }
-            }catch(e){
-                console.log(e);
+            } catch ( e ) {
+                console.log( e );
                 data = {};
             }
         }
         return data;
     }
-    $(document).ready(function(){
-        $('.bulkactions').append( '<button type="button" class="button button-primary hb-update-ordering">Update</button>' );
-        $(document).on('click', '.hb-update-ordering', function(){
-           $(this.form).append('<input type="hidden" name="action" value="hb-update-taxonomy" />').submit();
-        });
-    });
-})(jQuery);
+    $( document ).ready( function () {
+        $( '.bulkactions' ).append( '<button type="button" class="button button-primary hb-update-ordering">Update</button>' );
+        $( document ).on( 'click', '.hb-update-ordering', function () {
+            $( this.form ).append( '<input type="hidden" name="action" value="hb-update-taxonomy" />' ).submit();
+        } );
+    } );
+} )( jQuery );
