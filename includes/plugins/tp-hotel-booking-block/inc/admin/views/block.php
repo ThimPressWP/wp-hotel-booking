dd42fa5f54317c085c617be4e1faace7d401b511
haihld_58 <hoangleenhan@gmail.com>
2016-11-04 16:27:27 +0700

    Fix block room calender plan

--- includes/plugins/tp-hotel-booking-block/inc/admin/views/block.php
+++ includes/plugins/tp-hotel-booking-block/inc/admin/views/block.php
@@ -5,8 +5,8 @@
  */
 global $wpdb;
 $rooms = $wpdb->get_results( $wpdb->prepare(
-                "SELECT ID, post_title FROM {$wpdb->posts} WHERE `post_type` = %s AND `post_status` = %s", 'hb_room', 'publish'
-        ), OBJECT );
+    "SELECT ID, post_title FROM {$wpdb->posts} WHERE `post_type` = %s AND `post_status` = %s", 'hb_room', 'publish'
+), OBJECT );
 ?>
 
 <div class="wrap" ng-app="hotel_booking_block">
@@ -46,146 +46,146 @@ $rooms = $wpdb->get_results( $wpdb->prepare(
 
         app.controller( 'hotel_booking_calendar', [ '$scope', '$http', function ( $scope, $http ) {
 
-                $scope.rooms = <?php echo json_encode( $rooms ) ?>;
-                // moment.tz.setDefault( '<?php echo date_default_timezone_get() ?>' );
-                // $scope.today = moment().unix( <?php echo current_time( 'timestamp', 1 ) * 1000 ?> );
-                $scope.today = moment().utc();
-                // $scope.todaya = moment();
+            $scope.rooms = <?php echo json_encode( $rooms ) ?>;
+            // moment.tz.setDefault( '<?php echo date_default_timezone_get() ?>' );
+            // $scope.today = moment().unix( <?php echo current_time( 'timestamp', 1 ) * 1000 ?> );
+            $scope.today = moment().utc();
+            // $scope.todaya = moment();
 
-                $scope.calendar = [ ];
+            $scope.calendar = [ ];
+            $scope.message = '';
+            $scope.status_ajax = '';
+
+            $scope.temp = false;
+            $scope.addSelected = false;
+            $scope.removeSelected = false;
+
+            $scope.calendars = <?php echo json_encode( Hotel_Booking_Block::instance()->get_blocked(), true ) ?>;
+
+            // add new calendar
+            $scope.add_calendar = function () {
                 $scope.message = '';
-                $scope.status_ajax = '';
+                var unique_time = moment().valueOf(),
+                    new_calendar = {
+                        id: unique_time,
+                        post_id: [ ],
+                        selected: [ ]
+                    }
+                $scope.calendars[ unique_time ] = new_calendar;
+            };
 
-                $scope.temp = false;
-                $scope.addSelected = false;
-                $scope.removeSelected = false;
+            // save form
+            $scope.submit = function ( ) {
+                $scope.message = '';
 
-                $scope.calendars = <?php echo json_encode( Hotel_Booking_Block::instance()->get_blocked(), true ) ?>;
-
-                // add new calendar
-                $scope.add_calendar = function () {
-                    $scope.message = '';
-                    var unique_time = moment().valueOf(),
-                            new_calendar = {
-                                id: unique_time,
-                                post_id: [ ],
-                                selected: [ ]
-                            }
-                    $scope.calendars[ unique_time ] = new_calendar;
-                };
-
-                // save form
-                $scope.submit = function ( ) {
-                    $scope.message = '';
-
-                    $http.post(
-                            Hotel_Booking_Block.ajaxurl + '&action=hotel_block_update',
-                            {
-                                data: JSON.stringify( $scope.calendars )
-                            }
-                    ).success( function ( data, status, headers, config ) {
-                        var status_ajax = 'error';
-                        if ( status === 200 )
-                        {
-                            if ( typeof data.status === 'undefined' || data.status !== 'success' )
-                            {
-                                $scope.message = Hotel_Booking_Block.error_ajax;
-                            }
-
-                            if ( typeof data.data !== 'undefined' )
-                            {
-                                status_ajax = 'success';
-                                $scope.add_calendars = data.data;
-                                if ( typeof data.message !== 'undefined' )
-                                {
-                                    $scope.message = data.message;
-                                }
-                            }
-                        } else
+                $http.post(
+                    Hotel_Booking_Block.ajaxurl + '&action=hotel_block_update',
+                    {
+                        data: JSON.stringify( $scope.calendars )
+                    }
+                ).success( function ( data, status, headers, config ) {
+                    var status_ajax = 'error';
+                    if ( status === 200 )
+                    {
+                        if ( typeof data.status === 'undefined' || data.status !== 'success' )
                         {
                             $scope.message = Hotel_Booking_Block.error_ajax;
                         }
-                        $scope.status_ajax = status_ajax;
-                    } );
-                };
 
-                // on select calendar
-                $scope.logInfos = function ( time, date ) {
-                    var time = date.valueOf(); // time is timestamp utc
-                    $scope.temp = true;
-
-                    if ( date.selected === false )
-                    {
-                        $scope.removeSelected = false;
-                        $scope.addSelected = time;
-                    } else
-                    {
-                        $scope.addSelected = false;
-                        $scope.removeSelected = time;
-                    }
-                };
-
-                // add remove scope calendar
-                $scope.callback = function ( id ) {
-                    if ( $scope.temp === false )
-                        return;
-
-                    if ( typeof $scope.calendars[id].selected === 'undefined' )
-                    {
-                        $scope.calendars[id].selected = [ ];
-                    }
-                    if ( $scope.addSelected && $scope.removeSelected === false )
-                    {
-                        $scope.calendars[id].selected.push( $scope.addSelected )
-                    } else if ( $scope.removeSelected && $scope.addSelected === false )
-                    {
-                        var index = $scope.calendars[id].selected.indexOf( $scope.removeSelected );
-                        $scope.calendars[id].selected.splice( index, 1 );
-                    }
-
-                    $scope.temp = false;
-                    $scope.addSelected = false;
-                };
-
-                $scope.remove_calendar = function ( id ) {
-                    $scope.message = '';
-                    $http.post(
-                            Hotel_Booking_Block.ajaxurl + '&action=hotel_block_delete_post_type',
-                            {
-                                calendar_id: id
-                            },
-                            {
-                                async: false
-                            }
-                    ).success( function ( data, status, headers, config ) {
-                        var status_ajax = 'error';
-                        if ( status === 200 )
+                        if ( typeof data.data !== 'undefined' )
                         {
-                            if ( typeof data.status !== 'undefined' && data.status === 'success' )
-                            {
-                                status_ajax = 'success';
-                            }
-
+                            status_ajax = 'success';
+                            $scope.calendars = data.data;
                             if ( typeof data.message !== 'undefined' )
                             {
                                 $scope.message = data.message;
-                            } else
-                            {
-                                $scope.message = '';
                             }
+                        }
+                    } else
+                    {
+                        $scope.message = Hotel_Booking_Block.error_ajax;
+                    }
+                    $scope.status_ajax = status_ajax;
+                } );
+            };
 
-                            if ( typeof data.data !== 'undefined' )
-                            {
-                                $scope.calendars = data.data;
-                            }
+            // on select calendar
+            $scope.logInfos = function ( time, date ) {
+                var time = date.valueOf(); // time is timestamp utc
+                $scope.temp = true;
+
+                if ( date.selected === false )
+                {
+                    $scope.removeSelected = false;
+                    $scope.addSelected = time;
+                } else
+                {
+                    $scope.addSelected = false;
+                    $scope.removeSelected = time;
+                }
+            };
+
+            // add remove scope calendar
+            $scope.callback = function ( id ) {
+                if ( $scope.temp === false )
+                    return;
+
+                if ( typeof $scope.calendars[id].selected === 'undefined' )
+                {
+                    $scope.calendars[id].selected = [ ];
+                }
+                if ( $scope.addSelected && $scope.removeSelected === false )
+                {
+                    $scope.calendars[id].selected.push( $scope.addSelected )
+                } else if ( $scope.removeSelected && $scope.addSelected === false )
+                {
+                    var index = $scope.calendars[id].selected.indexOf( $scope.removeSelected );
+                    $scope.calendars[id].selected.splice( index, 1 );
+                }
+
+                $scope.temp = false;
+                $scope.addSelected = false;
+            };
+
+            $scope.remove_calendar = function ( id ) {
+                $scope.message = '';
+                $http.post(
+                    Hotel_Booking_Block.ajaxurl + '&action=hotel_block_delete_post_type',
+                    {
+                        calendar_id: id
+                    },
+                    {
+                        async: false
+                    }
+                ).success( function ( data, status, headers, config ) {
+                    var status_ajax = 'error';
+                    if ( status === 200 )
+                    {
+                        if ( typeof data.status !== 'undefined' && data.status === 'success' )
+                        {
+                            status_ajax = 'success';
+                        }
+
+                        if ( typeof data.message !== 'undefined' )
+                        {
+                            $scope.message = data.message;
                         } else
                         {
-                            $scope.message = Hotel_Booking_Block.error_ajax;
+                            $scope.message = '';
                         }
-                        $scope.status_ajax = status_ajax;
-                    } );
-                };
-            } ] );
+
+                        if ( typeof data.data !== 'undefined' )
+                        {
+                            $scope.calendars = data.data;
+                        }
+                    } else
+                    {
+                        $scope.message = Hotel_Booking_Block.error_ajax;
+                    }
+                    $scope.status_ajax = status_ajax;
+                } );
+            };
+        } ] );
 
         // directive
         var hotel_booking_directive = angular.module( 'hotel_booking_directive', [ ] );
