8ac02aa89d1e0622cccd81bfe2523940c50efa06
haihld_58 <hoangleenhan@gmail.com>
2016-11-24 09:19:27 +0700

    Fix min, max price in single room page

--- templates/loop/price.php
+++ templates/loop/price.php
@@ -2,34 +2,34 @@
 /**
  * Loop Price
  *
- * @author 		ThimPress
- * @package 	Tp-hotel-booking/Templates
- * @version     1.1.3
+ * @author        ThimPress
+ * @package       Tp-hotel-booking/Templates
+ * @version       1.1.3
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if ( !defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
 global $hb_settings;
-$price_display = apply_filters( 'hotel_booking_loop_room_price_display_style', $hb_settings->get('price_display') );
+$price_display = apply_filters( 'hotel_booking_loop_room_price_display_style', $hb_settings->get( 'price_display' ) );
 
 $prices = hb_room_get_selected_plan( get_the_ID() );
 $prices = isset( $prices->prices ) ? $prices->prices : array();
 
 ?>
-<?php if( $prices ): ?>
+<?php if ( $prices ): ?>
 	<?php
-		$min = current($prices);
-		$max = end($prices);
+	$min = min( $prices );
+	$max = max( $prices );
 	?>
 	<div class="price">
 		<span class="title-price"><?php _e( 'Price', 'tp-hotel-booking' ); ?></span>
-		<?php if( $price_display === 'max' ): ?>
+		<?php if ( $price_display === 'max' ): ?>
 
 			<span class="price_value price_max"><?php echo hb_format_price( $max ) ?></span>
 
-		<?php elseif( $price_display === 'min_to_max' && $min !== $max ): ?>
+		<?php elseif ( $price_display === 'min_to_max' && $min !== $max ): ?>
 
 			<span class="price_value price_min_to_max">
 				<?php echo hb_format_price( $min ) ?>
