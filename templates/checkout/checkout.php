a32a77a0d0af860cf9e512ff187640c0a881b1fe
Lee <hoangleenhan@gmail.com>
2017-04-19 16:16:11 +0700

    Update undefined variable checkout page

--- templates/checkout/checkout.php
+++ templates/checkout/checkout.php
@@ -77,6 +77,7 @@ do_action( 'hotel_booking_before_checkout_form' );
                         <span class="hb-align-right hb_grand_total_value"><?php echo hb_format_price( $cart->total ); ?></span>
                     </td>
                 </tr>
+				<?php $advance_payment = ''; ?>
 				<?php if ( $advance_payment == $cart->advance_payment ) { ?>
                     <tr class="hb_advance_payment">
                         <td colspan="8">
