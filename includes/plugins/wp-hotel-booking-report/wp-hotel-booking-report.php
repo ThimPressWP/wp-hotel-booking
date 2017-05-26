78e2cfef334ef555b6889c8bbcca6e5fcb559f21
Lee <hoangleenhan@gmail.com>
2017-05-26 13:19:52 +0700

    Fix export function of report add-on

--- includes/plugins/wp-hotel-booking-report/wp-hotel-booking-report.php
+++ includes/plugins/wp-hotel-booking-report/wp-hotel-booking-report.php
@@ -4,11 +4,11 @@
   Plugin URI: http://thimpress.com/
   Description: WP Hotel Booking Report
   Author: ThimPress
-  Version: 1.7
+  Version: 1.7.1
  */
 define( 'TP_HB_REPORT_DIR', plugin_dir_path( __FILE__ ) );
 define( 'TP_HB_REPORT_URI', plugin_dir_url( __FILE__ ) );
-define( 'TP_HB_REPORT_VER', '1.7' );
+define( 'TP_HB_REPORT_VER', '1.7.1' );
 
 class WP_Hotel_Booking_Report {
 
@@ -68,6 +68,10 @@ class WP_Hotel_Booking_Report {
 	function init() {
 		require_once TP_HB_REPORT_DIR . '/inc/class-hb-report-price.php';
 		require_once TP_HB_REPORT_DIR . '/inc/class-hb-report-room.php';
+
+		$report = HB_Report_Room::instance();
+
+		$report->export_csv();
 	}
 
 	/**
