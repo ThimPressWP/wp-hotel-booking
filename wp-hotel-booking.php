c00ad58cbe186a7c47de54d85c391dac56bf63ab
tungnx <tungnxt89@gmail.com>
2023-07-14 16:45:35 +0700

    = 2.0.6 = ~ Release.

--- wp-hotel-booking.php
+++ wp-hotel-booking.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://thimpress.com/
  * Description: Full of professional features for a booking room system
  * Author: ThimPress
- * Version: 2.0.6-beta-2
+ * Version: 2.0.6
  * Author URI: http://thimpress.com
  * Text Domain: wp-hotel-booking
  * Domain Path: /languages/
