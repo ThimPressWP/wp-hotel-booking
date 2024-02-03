a8347f771465e549bf243b5ae1f0005260b78675
Khanh Bui <khanhbd@physcode.com>
2024-02-03 17:27:03 +0700

    =2.0.9.4= ~ add paypal payment order api

--- includes/gateways/paypal/views/settings.php
+++ includes/gateways/paypal/views/settings.php
@@ -18,10 +18,13 @@ $paypal   = $settings->get( 'paypal' );
 $paypal   = wp_parse_args(
 	$paypal,
 	array(
-		'enable'        => 'on',
-		'email'         => '',
-		'sandbox'       => 'off',
-		'sandbox_email' => '',
+		'enable'            => 'on',
+		'email'             => '',
+		'sandbox'           => 'off',
+		'sandbox_email'     => '',
+		'app_client_id'     => '',
+		'app_client_secret' => '',
+		'use_paypal_rest'   => 'off',
 	)
 );
 
@@ -52,6 +55,42 @@ $field_name = $settings->get_field_name( 'paypal' );
 		<th><?php _e( 'Paypal sandbox email', 'wp-hotel-booking' ); ?></th>
 		<td>
 			<input type="email" class="regular-text" name="<?php echo esc_attr( $field_name ); ?>[sandbox_email]" value="<?php echo esc_attr( $paypal['sandbox_email'] ); ?>" />
+		</td>
+	</tr>
+	<tr>
+		<th><?php _e( 'Use PayPal REST API', 'wp-hotel-booking' ); ?></th>
+		<td>
+			<input type="hidden" name="<?php echo esc_attr( $field_name ); ?>[use_paypal_rest]" value="off" />
+			<input type="checkbox" name="<?php echo esc_attr( $field_name ); ?>[use_paypal_rest]" <?php checked( $paypal['use_paypal_rest'] == 'on' ? 1 : 0, 1 ); ?> value="on" />
+			<span class="description"><?php esc_html_e( 'Recommendations! The old standard will not be supported in 2024/01/31.', 'wp-hotel-booking' ); ?></span>
+		</td>
+	</tr>
+	<tr>
+		<th><?php _e( 'Client ID', 'wp-hotel-booking' ); ?></th>
+		<td>
+			<input type="text" class="regular-text" name="<?php echo esc_attr( $field_name ); ?>[app_client_id]" value="<?php echo esc_attr( $paypal['app_client_id'] ); ?>" />
+			<p class="description">
+			<?php
+			echo sprintf(
+				__( 'How to get <a href="%s" target="_blank">Client ID</a>', 'learnpress' ),
+				'https://developer.paypal.com/api/rest/#link-getclientidandclientsecret'
+			)
+			?>
+			</p>
+		</td>
+	</tr>
+	<tr>
+		<th><?php _e( 'Client Secret', 'wp-hotel-booking' ); ?></th>
+		<td>
+			<input type="text" class="regular-text" name="<?php echo esc_attr( $field_name ); ?>[app_client_secret]" value="<?php echo esc_attr( $paypal['app_client_secret'] ); ?>" />
+			<p class="description">
+			<?php
+			echo sprintf(
+				__( 'How to get <a href="%s" target="_blank">Client Secret</a>', 'learnpress' ),
+				'https://developer.paypal.com/api/rest/#link-getclientidandclientsecret'
+			)
+			?>
+			</p>
 		</td>
 	</tr>
 </table>
